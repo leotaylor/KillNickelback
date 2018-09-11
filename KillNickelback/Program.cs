0b5306601fa58a4cc7c6a3c81c90809c34d26b6e
Leo Taylor <leotaylor723@gmail.com>
2018-09-10 20:36:47 -0500

    prints all songs that are not nickelback

--- KillNickelback/Program.cs
+++ KillNickelback/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace KillNickelback
 {
@@ -6,7 +7,31 @@ namespace KillNickelback
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Song> goodSongs = new List<Song>();
+            HashSet<Song> allSongs = new HashSet<Song>();
+
+            allSongs.Add(new Song("Megadeth", "Hanger 18"));
+            allSongs.Add(new Song("Nickelback", "Photograph"));
+            allSongs.Add(new Song("Pink Floyd", "The Great Gig In The Sky"));
+            allSongs.Add(new Song("Nickelback", "Rockstar"));
+            allSongs.Add(new Song("Opeth", "Deliverance"));
+            allSongs.Add(new Song("Nickelback", "How You Remind Me"));
+            allSongs.Add(new Song("Judas Priest", "Painkiller"));
+
+            foreach(var song in allSongs)
+            {
+                if(song.Artist != "Nickelback")
+                {
+                    goodSongs.Add(song);
+                }
+            }
+
+            foreach(var song in goodSongs)
+            {
+                Console.WriteLine($"{song.Name} by {song.Artist} is a good song.");
+            }
+
+            Console.ReadLine();
         }
     }
 }
